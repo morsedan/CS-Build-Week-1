d6b7612484c0e74a917670d1d5c1db2ce1ac0652
Dan Morse <djmthedev@gmail.com>
2020-04-29 13:11:08 -0700

    changed max title length from 50 to 60

--- adventure/models.py
+++ adventure/models.py
@@ -7,7 +7,7 @@ import uuid
 
 class Room(models.Model):
     room_id = models.IntegerField()
-    title = models.CharField(max_length=50, default="DEFAULT TITLE")
+    title = models.CharField(max_length=60, default="DEFAULT TITLE")
     description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
     n_to = models.IntegerField(default=0)
     s_to = models.IntegerField(default=0)
